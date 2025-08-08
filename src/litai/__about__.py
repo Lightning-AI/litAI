c631757537d71f5131cb120e32a1b8796d4b7fd2
Kaeun Kim <kaeun@lightning.ai>
2025-08-08 10:16:44 +0100

    bump: 0.0.8

--- src/litai/__about__.py
+++ src/litai/__about__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __author__ = "Lightning-AI et al."
 __author_email__ = "community@lightning.ai"
 __copyright__ = f"Copyright (c) 2025, {__author__}."
