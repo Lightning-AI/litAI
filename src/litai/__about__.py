5c6f65ccf891b764b4bab400474d634b945b4d9c
Kaeun Kim <kaeun@lightning.ai>
2025-08-26 17:23:57 +0100

    Release 0.0.10 (#52)
    * bump: 0.0.10
    
    * bump: 0.0.10
    
    * fix: update

--- src/litai/__about__.py
+++ src/litai/__about__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 __author__ = "Lightning-AI et al."
 __author_email__ = "community@lightning.ai"
 __copyright__ = f"Copyright (c) 2025, {__author__}."
