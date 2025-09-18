a09cf3043db8f90022f37ba4d180c39a5f0dd741
Daniela Dapena <danidapena3093@gmail.com>
2025-09-18 14:21:19 +0000

    fix rebase

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -178,7 +178,7 @@ def test_reasoning_effort_override(mock_llm_class):
         full_response=False,
         my_kwarg="test-kwarg",
         tools=None,
-        reasoning_effort="none",
+        reasoning_effort=None,
     )
     test_kwargs = mock_llm_instance.chat.call_args.kwargs
     assert test_kwargs.get("my_kwarg") == "test-kwarg"
